8f4a0ccf210087a2286b4f0eafd20c86d264dd17
TheMostHigh <tallestlegacy@gmail.com>
2026-01-04 16:03:47 +0300

    wip: render square window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bevy::{prelude::*, window::WindowResolution};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Falppy Bird"),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: WindowResolution::new(512, 512),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup_level)
+        .run();
+}
+
+struct Bird {
+    pub velocity: f32,
+}
+
+fn setup_level(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
 }
