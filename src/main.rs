da1b353db4bba5cc0389f15553ab285aee514089
TheMostHigh <tallestlegacy@gmail.com>
2026-01-04 17:02:09 +0300

    wip: render pipes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::{
+    prelude::*,
+    window::{PrimaryWindow, WindowResolution},
+};
+use rand::{Rng, rng, rngs::ThreadRng};
 
-const PIXEL_RATIO: f32 = 4.0;
-const FLAP_FORCE: f32 = 500.0;
-const GRAVITY: f32 = 2000.0;
+const PIXEL_RATIO: f32 = 4.;
+const FLAP_FORCE: f32 = 500.;
+const GRAVITY: f32 = 2000.;
 const VELOCITY_TO_ROTATION_RATION: f32 = 7.5;
+
+const OBSTACLE_AMOUNT: i32 = 5;
+const OBSTACLE_WIDTH: f32 = 32.;
+const OBSTACLE_HEIGHT: f32 = 144.;
+const OBSTACLE_VERTICAL_OFFSET: f32 = 30.;
+const OBSTACLE_GAP_SIZE: f32 = 15.;
+const OBSTACLE_SPACING: f32 = 60.;
+const OBSTACLE_SCROLL_SPEED: f32 = 150.;
+
+#[derive(Component)]
+struct Bird {
+    pub velocity: f32,
+}
+
+#[derive(Component)]
+struct Obstacle {
+    pub pipe_direction: f32,
+}
+
+#[derive(Resource)]
+struct GameManager {
+    pub pipe_image: Handle<Image>,
+    pub window_dimenstions: Vec2,
+}
 
 fn main() {
     App::new()
@@ -25,12 +53,18 @@ fn main() {
         .run();
 }
 
-#[derive(Component)]
-struct Bird {
-    pub velocity: f32,
-}
+fn setup_level(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let pipe_image = asset_server.load("pipe.png");
+    let window = window_query.single().unwrap();
+    commands.insert_resource(GameManager {
+        pipe_image: pipe_image.clone(),
+        window_dimenstions: Vec2::new(window.width(), window.height()),
+    });
 
-fn setup_level(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
     commands.spawn(Camera2d::default());
     commands.spawn((
@@ -41,6 +75,9 @@ fn setup_level(mut commands: Commands, asset_server: Res<AssetServer>) {
         Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
         Bird { velocity: 0. },
     ));
+
+    let mut rand = rng();
+    spawn_obstacles(&mut commands, &mut rand, window.width(), &pipe_image);
 }
 
 fn update_bird(
@@ -59,4 +96,56 @@ fn update_bird(
             f32::clamp(bird.velocity / VELOCITY_TO_ROTATION_RATION, -90.0, 90.0).to_radians(),
         )
     }
+}
+
+fn spawn_obstacles(
+    mut commands: &mut Commands,
+    mut rand: &mut ThreadRng,
+    window_width: f32,
+    pipe_image: &Handle<Image>,
+) {
+    for i in 0..OBSTACLE_AMOUNT {
+        let y_offset = generate_offset(rand);
+        let x_pos = window_width / 2. + (OBSTACLE_SPACING * PIXEL_RATIO * i as f32);
+        spawn_obstacle(
+            Vec3::X * x_pos + Vec3::Y * (get_centered_position() + y_offset),
+            1.,
+            commands,
+            pipe_image,
+        );
+        spawn_obstacle(
+            Vec3::X * x_pos + Vec3::Y * (-get_centered_position() + y_offset),
+            -1.,
+            commands,
+            pipe_image,
+        );
+    }
+}
+
+fn spawn_obstacle(
+    translation: Vec3,
+    pipe_direction: f32,
+    commands: &mut Commands,
+    pipe_image: &Handle<Image>,
+) {
+    commands.spawn((
+        Sprite {
+            image: pipe_image.clone(),
+            ..Default::default()
+        },
+        Transform::from_translation(translation).with_scale(Vec3::new(
+            PIXEL_RATIO,
+            PIXEL_RATIO * -pipe_direction,
+            PIXEL_RATIO,
+        )),
+        Obstacle { pipe_direction },
+    ));
+}
+
+fn generate_offset(rand: &mut ThreadRng) -> f32 {
+    rand.gen_range(-OBSTACLE_VERTICAL_OFFSET..OBSTACLE_VERTICAL_OFFSET) * PIXEL_RATIO
+}
+
+fn get_centered_position() -> f32 {
+    (OBSTACLE_HEIGHT / 2.0 + OBSTACLE_GAP_SIZE) * PIXEL_RATIO
 }
